2ec4943eafa6a3c9bd37df9ccccbb28935d4faa9
Júnior Nunes <denisonprobable13@gmail.com>
2021-04-13 15:45:04 -0300

    Hello-World successfully executed

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 const http = require('http');
+
+http.createServer((req, res) => {
+
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end("Hello-World");
+
+}).listen(3000, err => {
+    err ? console.log(err) : console.log("Servidor rodando na porta 3000")
+});
