ada140b1c1838ff03a1843f6089d7ee7b2356e26
Júnior Nunes <denisonprobable13@gmail.com>
2021-04-13 15:51:25 -0300

    Content-type application/json

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const http = require('http');
 
 http.createServer((req, res) => {
 
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end("<h1>Hello-World</h1>");
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ texto: "<h1>Hello-World</h1>" }));
 
 }).listen(3000, err => {
     err ? console.log(err) : console.log("Servidor rodando na porta 3000")
