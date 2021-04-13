44a3ce7b981394291124cefb590feecf7b08e9e0
Júnior Nunes <denisonprobable13@gmail.com>
2021-04-13 18:42:08 -0300

    Automatic targeting for index.html

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 const http = require('http');
+const url = require('url');
+const fs = require('fs');
 
 http.createServer((req, res) => {
 
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.end(JSON.stringify({ texto: "<h1>Hello-World</h1>" }));
+    path = url.parse(req.url).pathname;
+    path = '' || '/' ? '/index.html' : path;
+    const fileName = "." + path;
+
+    fs.readFile(fileName, (err, data) => {
+        if(err){
+            res.writeHead(404, { "Content-Type": "text/html;charset=utf-8"});
+            res.end("<h1>Page not found</h1>")
+        }else{
+            res.writeHead(200, { "Content-Type": "text/html"});
+            res.write(data);
+            res.end();
+        }
+
+    });
 
 }).listen(3000, err => {
     err ? console.log(err) : console.log("Servidor rodando na porta 3000")
