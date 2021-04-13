a8b9d2530edd301199dfa7b1702e7a7e19660ee1
Júnior Nunes <denisonprobable13@gmail.com>
2021-04-13 15:48:15 -0300

    content-type: text/html

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const http = require('http');
 
 http.createServer((req, res) => {
 
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end("Hello-World");
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end("<h1>Hello-World</h1>");
 
 }).listen(3000, err => {
     err ? console.log(err) : console.log("Servidor rodando na porta 3000")
-});
+}); 
