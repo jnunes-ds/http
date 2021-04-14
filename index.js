2898e72ae59cd6877d55bb415865ce55f4aeacfc
Júnior Nunes <denisonprobable13@gmail.com>
2021-04-13 22:37:41 -0300

    server prototype

--- index.js
+++ index.js
@@ -2,16 +2,21 @@ const http = require('http');
 const url = require('url');
 const fs = require('fs');
 
-http.createServer((req, res) => {
 
-    path = url.parse(req.url).pathname;
-    path = '' || '/' ? '/index.html' : path;
+function handleFile(req, res, callback) {
+    let path = url.parse(req.url).pathname;
+    path = path === ('' || '/') ? '/index.html' : path;
     const fileName = "." + path;
 
     fs.readFile(fileName, (err, data) => {
         if(err){
-            res.writeHead(404, { "Content-Type": "text/html;charset=utf-8"});
-            res.end("<h1>Page not found</h1>")
+
+            if(callback){
+                if(!callback(req, res)){
+                    res.writeHead(404, { "Content-Type": "text/html;charset=utf-8"});
+                    res.end("<h1>Page not found</h1>")
+                };
+            }
         }else{
             res.writeHead(200, { "Content-Type": "text/html"});
             res.write(data);
@@ -19,6 +24,25 @@ http.createServer((req, res) => {
         }
 
     });
+};
+
+function handleRequest(req, res){
+    let path = url.parse(req.url).pathname;
+    path = path === ('' || '/') ? '/index.html' : path;
+
+    if(path === '/test'){
+        res.end("Test");
+        return true;
+    };
+
+    return false;
+};
+
+
+http.createServer((request, response) => {
+
+    handleFile(request, response, handleRequest);
+    
 
 }).listen(3000, err => {
     err ? console.log(err) : console.log("Servidor rodando na porta 3000")
